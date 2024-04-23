a2bd787fa71d3d3b759da5dc139b759843ee3bec
Ytherambugua <mbugualinnete@gmail.com>
2024-04-23 00:49:48 -0700

    API and Everything

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
 """
-Script that, using a given REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Script to retrieve TODO list progress of an employee from a given REST API.
 """
 
 import requests
 import sys
 
-def fetch_employee_todo_list(employee_id):
+def get_employee_todo_progress(employee_id):
     """
-    Fetches the TODO list of a given employee from the API.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        dict: The JSON response containing the TODO list data.
+    Retrieves and displays the TODO list progress of a given employee.
     """
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
-    response = requests.get(url)
-    return response.json()
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
 
-def display_todo_progress(employee_id, todo_list):
-    """
-    Displays the progress of the TODO list for a given employee.
+    try:
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+        employee_name = user_data.get('name')
 
-    Args:
-        employee_id (int): The ID of the employee.
-        todo_list (list): The TODO list data.
-    """
-    employee_name = todo_list[0]['username']
-    total_tasks = len(todo_list)
-    completed_tasks = [task for task in todo_list if task['completed']]
-    num_completed_tasks = len(completed_tasks)
+        todos_response = requests.get(todos_url)
+        todos_data = todos_response.json()
+        total_tasks = len(todos_data)
+        completed_tasks = [task for task in todos_data if task.get('completed')]
+        num_completed_tasks = len(completed_tasks)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
-    for task in completed_tasks:
-        print("\t", task['title'])
+        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+        for task in completed_tasks:
+            print(f"\t{task.get('title')}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+    
+    employee_id = sys.argv[1]
+    if not employee_id.isdigit():
+        print("Employee ID must be an integer.")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    todo_list = fetch_employee_todo_list(employee_id)
-    display_todo_progress(employee_id, todo_list)
+    get_employee_todo_progress(employee_id)
 
